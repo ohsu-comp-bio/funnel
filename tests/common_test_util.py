f3d67e815f29c061ea75c1865502eb3c88517206
Alex Buchanan <buchanae@ohsu.edu>
2016-12-16 11:51:18 -0800

    Add python deps. to travisCI config

--- tests/common_test_util.py
+++ tests/common_test_util.py
@@ -34,7 +34,7 @@ class SimpleServerTest(unittest.TestCase):
         time.sleep(3)
         
         self.task_worker = None
-        cmd = ["./bin/tes-worker", "-volumes", self.volume_dir]
+        cmd = ["./bin/tes-worker", "-workdir", self.volume_dir]
         logging.info("Running %s" % (" ".join(cmd)))
         self.task_worker = subprocess.Popen(cmd)
 

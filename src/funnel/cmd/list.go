f58087e4ba21a2018a2a3eee9751a2b8ed1792a1
adamstruck <strucka@ohsu.edu>
2017-04-03 13:11:30 -0700

    filter job list on name and state

--- src/funnel/cmd/list.go
+++ src/funnel/cmd/list.go
@@ -1,17 +1,18 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/elgs/jsonql"
 	"github.com/spf13/cobra"
 	"net/http"
+	"os"
+	"strings"
 )
 
 var (
 	taskName  string
-	taskDesc  string
-	taskTags  string
-	taskStart string
-	taskEnd   string
+	taskState string
 )
 
 // listCmd represents the list command
@@ -21,10 +22,43 @@ var listCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		resp, err := http.Get(tesServer + "/v1/jobs")
 		body := responseChecker(resp, err)
-		fmt.Printf("%s", body)
+
+		var jobList map[string]interface{}
+		err = json.Unmarshal(body, &jobList)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		jobs := jobList["jobs"]
+		parser := jsonql.NewQuery(jobs)
+		var queries []string
+
+		if taskState != "" {
+			queries = append(queries, fmt.Sprintf("state~='%s'", taskState))
+		}
+		if taskName != "" {
+			queries = append(queries, fmt.Sprintf("task.name~='%s'", taskName))
+		}
+		if len(queries) > 0 {
+			jobs, err = parser.Query(strings.Join(queries, " && "))
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		}
+
+		jobsJson, err := json.Marshal(jobs)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", jobsJson)
 	},
 }
 
 func init() {
 	taskCmd.AddCommand(listCmd)
+	listCmd.Flags().StringVarP(&taskState, "state", "s", "", "Task state")
+	listCmd.Flags().StringVarP(&taskName, "name", "n", "", "Task name")
 }

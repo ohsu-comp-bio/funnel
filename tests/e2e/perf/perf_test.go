d2893f9217f6aa050741c387bdc7f050aba2e464
Alex Buchanan <buchanae@ohsu.edu>
2017-06-07 10:18:14 -0700

    Initial, simple performance benchmark

---
+++ tests/e2e/perf/perf_test.go
@@ -0,0 +1,45 @@
+package perf
+
+import (
+	"github.com/ohsu-comp-bio/funnel/tests/e2e"
+	"sync"
+	"testing"
+)
+
+func BenchmarkRun5000NoWorkers(b *testing.B) {
+	fun := e2e.NewFunnel()
+	// No workers connected in this test
+	fun.Conf.Scheduler = "manual"
+	fun.StartServer()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 5000; j++ {
+			fun.Run(`
+        --cmd 'echo'
+      `)
+		}
+	}
+}
+
+func BenchmarkRun5000ConcurrentNoWorkers(b *testing.B) {
+	fun := e2e.NewFunnel()
+	// No workers connected in this test
+	fun.Conf.Scheduler = "manual"
+	fun.StartServer()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		for j := 0; j < 5000; j++ {
+			wg.Add(1)
+			go func() {
+				wg.Done()
+				fun.Run(`
+          --cmd 'echo'
+        `)
+			}()
+		}
+		wg.Wait()
+	}
+}

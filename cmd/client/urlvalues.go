5cb1485cf9c68af6c1aaad9a5ac4ba2cdc8b3d79
Alex Buchanan <buchanae@ohsu.edu>
2017-06-23 14:49:40 -0700

    List all pages in cmd client and dashboards

---
+++ cmd/client/urlvalues.go
@@ -0,0 +1,17 @@
+package client
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func addString(u url.Values, key, value string) {
+	if value != "" {
+		u.Add(key, value)
+	}
+}
+func addUInt32(u url.Values, key string, value uint32) {
+	if value != 0 {
+		u.Add(key, fmt.Sprint(value))
+	}
+}

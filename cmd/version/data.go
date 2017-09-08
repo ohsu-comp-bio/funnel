b7d525d83bd74c9eefd7d8bc9f932b75c4d54812
Alex Buchanan <buchanae@ohsu.edu>
2017-09-07 18:28:38 -0700

    Add versioning info to build, cmds, and logs

---
+++ cmd/version/data.go
@@ -0,0 +1,9 @@
+package version
+
+// Build and version details
+const (
+	GitCommit = "ef02e23"
+	GitBranch = "versioning"
+	BuildDate = "Fri Sep  8 13:21:21 PDT 2017"
+	Version   = "0.2.0-20-gef02e23-dirty"
+)

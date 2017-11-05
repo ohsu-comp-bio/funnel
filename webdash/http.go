bce9970841683837462d6cc399f3cba7f4676070
Alex Buchanan <buchanae@ohsu.edu>
2017-11-05 14:23:31 -0800

    webdash: TES API urls in the browser dashboard

--- webdash/http.go
+++ webdash/http.go
@@ -5,14 +5,23 @@ import (
 	"net/http"
 )
 
+var fs = &assetfs.AssetFS{
+	Asset:     Asset,
+	AssetDir:  AssetDir,
+	AssetInfo: AssetInfo,
+	Prefix:    "webdash",
+}
+var index = MustAsset("webdash/index.html")
+
 // FileServer provides access to the bundled web assets (HTML, CSS, etc)
 // via an http.Handler
 func FileServer() http.Handler {
-	fs := &assetfs.AssetFS{
-		Asset:     Asset,
-		AssetDir:  AssetDir,
-		AssetInfo: AssetInfo,
-		Prefix:    "webdash",
-	}
 	return http.FileServer(fs)
+}
+
+// RootHandler returns an http handler which always responds with the /index.html file.
+func RootHandler() http.Handler {
+	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		resp.Write(index)
+	})
 }

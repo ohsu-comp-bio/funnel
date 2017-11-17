5cf4507188ec508ad5e729020dd1d26fad544372
adamstruck <strucka@ohsu.edu>
2017-11-17 10:26:05 -0800

    termdash: err handling in expanded view

--- cmd/termdash/config/main.go
+++ cmd/termdash/config/main.go
@@ -3,11 +3,6 @@
 
 package config
 
-import (
-	"fmt"
-	"os"
-)
-
 var (
 	GlobalParams   []*Param
 	GlobalSwitches []*Switch
@@ -20,17 +15,4 @@ func Init() {
 	for _, s := range switches {
 		GlobalSwitches = append(GlobalSwitches, s)
 	}
-}
-
-func quote(s string) string {
-	return fmt.Sprintf("\"%s\"", s)
-}
-
-// Return env var value if set, else return defaultVal
-func getEnv(key, defaultVal string) string {
-	val := os.Getenv(key)
-	if val != "" {
-		return val
-	}
-	return defaultVal
 }

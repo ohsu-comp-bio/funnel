a56754f7878318f0928de8fa097930cc2ee56239
Alex Buchanan <buchanae@ohsu.edu>
2018-07-09 09:57:07 -0700

    worker: file/string task readers

---
+++ worker/base64_taskreader.go
@@ -0,0 +1,42 @@
+package worker
+
+import (
+  "bytes"
+	"context"
+  "encoding/base64"
+	"fmt"
+
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/ohsu-comp-bio/funnel/tes"
+)
+
+type Base64TaskReader struct {
+	task *tes.Task
+}
+
+func NewBase64TaskReader(raw string) (*Base64TaskReader, error) {
+  data, err := base64.StdEncoding.DecodeString(raw)
+	if err != nil {
+		return nil, fmt.Errorf("decoding task: %v", err)
+	}
+
+	task := &tes.Task{}
+  buf := bytes.NewBuffer(data)
+	err = jsonpb.Unmarshal(buf, task)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshaling task: %v", err)
+	}
+  return &Base64TaskReader{task}, nil
+}
+
+// Task returns the task. A random ID will be generated.
+func (f *Base64TaskReader) Task(ctx context.Context) (*tes.Task, error) {
+	return f.task, nil
+}
+
+// State returns the task state. Due to some quirks in the implementation
+// of this reader, and since there is no online database to connect to,
+// this will always return QUEUED.
+func (f *Base64TaskReader) State(ctx context.Context) (tes.State, error) {
+	return f.task.GetState(), nil
+}
--- worker/file_taskreader.go
+++ worker/file_taskreader.go
@@ -11,59 +11,38 @@ import (
 
 // FileTaskReader provides a TaskReader implementation from a task file.
 type FileTaskReader struct {
-	Path string
 	task *tes.Task
 }
 
-// Task returns the task. A random ID will be generated.
-func (f *FileTaskReader) Task(ctx context.Context) (*tes.Task, error) {
-	if f.task != nil {
-		return f.task, nil
-	}
-
-	err := f.load()
+func NewFileTaskReader(path string) (*FileTaskReader, error) {
+	fh, err := os.Open(path)
 	if err != nil {
-		return nil, err
-	}
-
-	return f.task, nil
-}
-
-// State returns the task state. Due to some quirks in the implementation
-// of this reader, this will always return QUEUED.
-func (f *FileTaskReader) State(ctx context.Context) (tes.State, error) {
-	// TODO tes.InitTask is being used internally, which initializes the
-	//      task to QUEUED. So this will always return queued.
-
-	if f.task != nil {
-		return f.task.State, nil
-	}
-
-	err := f.load()
-	if err != nil {
-		return tes.Unknown, err
-	}
-
-	return f.task.State, nil
-}
-
-func (f *FileTaskReader) load() error {
-	fh, err := os.Open(f.Path)
-	if err != nil {
-		return fmt.Errorf("opening task file: %v", err)
+		return nil, fmt.Errorf("opening task file: %v", err)
 	}
 	defer fh.Close()
 
 	task := &tes.Task{}
 	err = jsonpb.Unmarshal(fh, task)
 	if err != nil {
-		return fmt.Errorf("unmarshaling task file: %v", err)
-	}
-	err = tes.InitTask(task)
-	if err != nil {
-		return fmt.Errorf("validating task: %v", err)
+		return nil, fmt.Errorf("unmarshaling task file: %v", err)
 	}
 
-	f.task = task
-	return nil
+	err = tes.InitTask(task)
+	if err != nil {
+		return nil, fmt.Errorf("validating task: %v", err)
+	}
+
+  return &FileTaskReader{task}, nil
+}
+
+// Task returns the task. A random ID will be generated.
+func (f *FileTaskReader) Task(ctx context.Context) (*tes.Task, error) {
+	return f.task, nil
+}
+
+// State returns the task state. Due to some quirks in the implementation
+// of this reader, and since there is no online database to connect to,
+// this will always return QUEUED.
+func (f *FileTaskReader) State(ctx context.Context) (tes.State, error) {
+  return f.task.GetState(), nil
 }

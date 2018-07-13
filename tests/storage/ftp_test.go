959c76d510fde3f9bdf7627fe607b4e69a85a3b2
Alex Buchanan <buchanae@ohsu.edu>
2018-07-13 16:44:07 -0700

    storage/ftp: add support for ftp auth config

--- tests/storage/ftp_test.go
+++ tests/storage/ftp_test.go
@@ -194,3 +194,187 @@ func TestFTPStorage(t *testing.T) {
 		t.Fatal("Expected warning in system logs")
 	}
 }
+
+// Test FTP storage with auth configured via Funnel config.
+func TestFTPStorageConfigAuth(t *testing.T) {
+	tests.SetLogOutput(log, t)
+	defer os.RemoveAll("./test_tmp")
+	defer os.RemoveAll("../ftp-test-server/_scratch/ftp-test/bob/test-output-directory")
+	defer os.RemoveAll("../ftp-test-server/_scratch/ftp-test/bob/test-output-file.txt")
+	defer os.RemoveAll("../ftp-test-server/_scratch/ftp-test/bob/testdata")
+
+	if !conf.FTPStorage.Valid() {
+		t.Skipf("Skipping FTP storage e2e tests...")
+	}
+	conf.FTPStorage.User = "bob"
+	conf.FTPStorage.Password = "bob"
+
+	ev := events.NewTaskWriter("test-task", 0, &events.Logger{Log: log})
+	testBucket := "localhost:8021"
+	ctx := context.Background()
+
+	protocol := "ftp://"
+
+	store, err := storage.NewMux(conf)
+	if err != nil {
+		t.Fatal("error configuring storage:", err)
+	}
+
+	fPath := "testdata/test_in"
+	inFileURL := protocol + testBucket + "/" + fPath
+	_, err = worker.UploadOutputs(ctx, []*tes.Output{
+		{Url: inFileURL, Path: fPath},
+	}, store, ev)
+	if err != nil {
+		t.Fatal("error uploading test file:", err)
+	}
+
+	dPath := "testdata/test_dir"
+	inDirURL := protocol + testBucket + "/" + dPath
+	_, err = worker.UploadOutputs(ctx, []*tes.Output{
+		{Url: inDirURL, Path: dPath, Type: tes.Directory},
+	}, store, ev)
+	if err != nil {
+		t.Fatal("error uploading test directory:", err)
+	}
+
+	outFileURL := protocol + testBucket + "/" + "test-output-file.txt"
+	outDirURL := protocol + testBucket + "/" + "test-output-directory/subdir"
+
+	task := &tes.Task{
+		Name: "storage e2e",
+		Inputs: []*tes.Input{
+			{
+				Url:  inFileURL,
+				Path: "/opt/inputs/test-file.txt",
+				Type: tes.FileType_FILE,
+			},
+			{
+				Url:  inDirURL,
+				Path: "/opt/inputs/test-directory",
+				Type: tes.FileType_DIRECTORY,
+			},
+		},
+		Outputs: []*tes.Output{
+			{
+				Path: "/opt/workdir/test-output-file.txt",
+				Url:  outFileURL,
+				Type: tes.FileType_FILE,
+			},
+			{
+				Path: "/opt/workdir/test-output-directory",
+				Url:  outDirURL,
+				Type: tes.FileType_DIRECTORY,
+			},
+		},
+		Executors: []*tes.Executor{
+			{
+				Image: "alpine:latest",
+				Command: []string{
+					"sh",
+					"-c",
+					"cat $(find /opt/inputs -type f | sort) > test-output-file.txt; mkdir test-output-directory; cp *.txt test-output-directory/",
+				},
+				Workdir: "/opt/workdir",
+			},
+		},
+	}
+
+	resp, err := fun.RPC.CreateTask(ctx, task)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	taskFinal := fun.Wait(resp.Id)
+
+	if taskFinal.State != tes.State_COMPLETE {
+		t.Fatal("Unexpected task failure")
+	}
+
+	expected := "file1 content\nfile2 content\nhello\n"
+
+	err = worker.DownloadInputs(ctx, []*tes.Input{
+		{Url: outFileURL, Path: "./test_tmp/test-gs-file.txt"},
+	}, store, ev)
+	if err != nil {
+		t.Fatal("Failed to download file:", err)
+	}
+
+	b, err := ioutil.ReadFile("./test_tmp/test-gs-file.txt")
+	if err != nil {
+		t.Fatal("Failed to read downloaded file:", err)
+	}
+	actual := string(b)
+
+	if actual != expected {
+		t.Log("expected:", expected)
+		t.Log("actual:  ", actual)
+		t.Fatal("unexpected content")
+	}
+
+	err = worker.DownloadInputs(ctx, []*tes.Input{
+		{Url: outDirURL, Path: "./test_tmp/test-gs-directory", Type: tes.Directory},
+	}, store, ev)
+	if err != nil {
+		t.Fatal("Failed to download directory:", err)
+	}
+
+	b, err = ioutil.ReadFile("./test_tmp/test-gs-directory/test-output-file.txt")
+	if err != nil {
+		t.Fatal("Failed to read file in downloaded directory", err)
+	}
+	actual = string(b)
+
+	if actual != expected {
+		t.Log("expected:", expected)
+		t.Log("actual:  ", actual)
+		t.Fatal("unexpected content")
+	}
+
+	// should succeed with warning when there is an input or output directory that
+	// does not exist
+	task = &tes.Task{
+		Name: "storage e2e",
+		Inputs: []*tes.Input{
+			{
+				Url:  protocol + testBucket + "/this/path/does/not/exist",
+				Path: "/opt/inputs/test-directory",
+				Type: tes.FileType_DIRECTORY,
+			},
+		},
+		Outputs: []*tes.Output{
+			{
+				Path: "/opt/workdir/this/path/does/not/exist/test-output-directory",
+				Url:  outDirURL,
+				Type: tes.FileType_DIRECTORY,
+			},
+		},
+		Executors: []*tes.Executor{
+			{
+				Image: "alpine:latest",
+				Command: []string{
+					"sleep", "1",
+				},
+			},
+		},
+	}
+
+	resp, err = fun.RPC.CreateTask(ctx, task)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	taskFinal = fun.Wait(resp.Id)
+	if taskFinal.State != tes.State_COMPLETE {
+		t.Fatal("Expected task failure")
+	}
+	found := false
+	for _, log := range taskFinal.Logs[0].SystemLogs {
+		if strings.Contains(log, "level='warning'") {
+			found = true
+		}
+	}
+	if !found {
+		t.Fatal("Expected warning in system logs")
+	}
+}

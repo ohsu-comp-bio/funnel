e012d2f8c79892677679a3730ad3b286c28c0f17
Liam Beckman <lbeckman314@gmail.com>
2024-07-26 16:06:02 -0700

    Add comments and clean up debug statements

--- worker/docker.go
+++ worker/docker.go
@@ -130,7 +130,6 @@ func (docker *Docker) SetIO(stdin io.Reader, stdout io.Writer, stderr io.Writer)
 
 // inspectContainer inspects the docker container for metadata.
 func (docker *Docker) InspectContainer(ctx context.Context) ContainerConfig {
-	fmt.Println("DEBUG: InspectContainer()")
 	// Give the container time to start.
 	time.Sleep(2 * time.Second)
 
@@ -141,7 +140,6 @@ func (docker *Docker) InspectContainer(ctx context.Context) ContainerConfig {
 	for i := 0; i < 5; i++ {
 		select {
 		case <-ctx.Done():
-			fmt.Println("DEBUG: ctx.Done()")
 			return ContainerConfig{}
 		case <-ticker.C:
 			cmd := exec.CommandContext(ctx, "docker", "inspect", docker.Name)

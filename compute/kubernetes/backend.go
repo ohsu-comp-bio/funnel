a095c3c647d83db3f4ebd99934ceacd9361df73a
Liam Beckman <lbeckman314@gmail.com>
2025-03-27 15:47:49 -0700

    Add initial plugin support to Kuberneterest backend

--- compute/kubernetes/backend.go
+++ compute/kubernetes/backend.go
@@ -21,42 +21,44 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
+	"github.com/imdario/mergo"
 	"github.com/ohsu-comp-bio/funnel/config"
 	"github.com/ohsu-comp-bio/funnel/events"
 	"github.com/ohsu-comp-bio/funnel/logger"
+	"github.com/ohsu-comp-bio/funnel/plugins/shared"
 	"github.com/ohsu-comp-bio/funnel/tes"
 )
 
 // NewBackend returns a new local Backend instance.
-func NewBackend(ctx context.Context, conf config.Kubernetes, reader tes.ReadOnlyServer, writer events.Writer, log *logger.Logger) (*Backend, error) {
-	if conf.TemplateFile != "" {
-		content, err := os.ReadFile(conf.TemplateFile)
+func NewBackend(ctx context.Context, conf config.Config, reader tes.ReadOnlyServer, writer events.Writer, log *logger.Logger) (*Backend, error) {
+	if conf.Kubernetes.TemplateFile != "" {
+		content, err := os.ReadFile(conf.Kubernetes.TemplateFile)
 		if err != nil {
 			return nil, fmt.Errorf("reading template: %v", err)
 		}
-		conf.Template = string(content)
+		conf.Kubernetes.Template = string(content)
 	}
-	if conf.Template == "" {
+	if conf.Kubernetes.Template == "" {
 		return nil, fmt.Errorf("invalid configuration; must provide a kubernetes job template")
 	}
-	if conf.Namespace == "" {
+	if conf.Kubernetes.Namespace == "" {
 		return nil, fmt.Errorf("invalid configuration; must provide a kubernetes namespace")
 	}
 
 	var kubeconfig *rest.Config
 	var err error
 
-	if conf.ConfigFile != "" {
+	if conf.Kubernetes.ConfigFile != "" {
 		// use the current context in kubeconfig
-		kubeconfig, err = clientcmd.BuildConfigFromFlags("", conf.ConfigFile)
+		kubeconfig, err = clientcmd.BuildConfigFromFlags("", conf.Kubernetes.ConfigFile)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("building kubeconfig: %v", err)
 		}
 	} else {
 		// creates the in-cluster config
 		kubeconfig, err = rest.InClusterConfig()
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("building in-cluster kubeconfig: %v", err)
 		}
 	}
 
@@ -67,28 +69,30 @@ func NewBackend(ctx context.Context, conf config.Kubernetes, reader tes.ReadOnly
 	}
 
 	b := &Backend{
-		bucket:      conf.Bucket,
-		region:      conf.Region,
-		client:      clientset.BatchV1().Jobs(conf.Namespace),
-		namespace:   conf.Namespace,
-		template:    conf.Template,
-		pvTemplate:  conf.PVTemplate,
-		pvcTemplate: conf.PVCTemplate,
-		event:       writer,
-		database:    reader,
-		log:         log,
-		config:      kubeconfig,
+		bucket:            conf.Kubernetes.Bucket,
+		region:            conf.Kubernetes.Region,
+		client:            clientset.BatchV1().Jobs(conf.Kubernetes.Namespace),
+		namespace:         conf.Kubernetes.Namespace,
+		template:          conf.Kubernetes.Template,
+		pvTemplate:        conf.Kubernetes.PVTemplate,
+		pvcTemplate:       conf.Kubernetes.PVCTemplate,
+		configMapTemplate: conf.Kubernetes.ConfigMapTemplate,
+		event:             writer,
+		database:          reader,
+		log:               log,
+		kubeconfig:        kubeconfig, // Kubernetes client config
+		conf:              conf,       // Funnel configuration
 	}
 
-	if !conf.DisableReconciler {
-		rate := time.Duration(conf.ReconcileRate)
-		go b.reconcile(ctx, rate, conf.DisableJobCleanup)
+	if !conf.Kubernetes.DisableReconciler {
+		rate := time.Duration(conf.Kubernetes.ReconcileRate)
+		go b.reconcile(ctx, rate, conf.Kubernetes.DisableJobCleanup)
 	}
 
 	return b, nil
 }
 
-// Backend represents the local backend.
+// Backend represents the K8s backend.
 type Backend struct {
 	bucket            string
 	region            string
@@ -97,11 +101,13 @@ type Backend struct {
 	template          string
 	pvTemplate        string
 	pvcTemplate       string
+	configMapTemplate string
 	event             events.Writer
 	database          tes.ReadOnlyServer
 	log               *logger.Logger
 	backendParameters map[string]string
-	config            *rest.Config
+	kubeconfig        *rest.Config  // Kubernetes client config
+	conf              config.Config // Funnel configuration
 	events.Computer
 }
 
@@ -124,8 +130,17 @@ func (b Backend) CheckBackendParameterSupport(task *tes.Task) error {
 // WriteEvent writes an event to the compute backend.
 // Currently, only TASK_CREATED is handled, which calls Submit.
 func (b *Backend) WriteEvent(ctx context.Context, ev *events.Event) error {
+	// TODO: Should this be moved to the switch statement so it's only run on TASK_CREATED?
+	if !b.conf.Plugins.Disabled {
+		err := b.UpdateConfig(ctx)
+		if err != nil {
+			return fmt.Errorf("error updating config from plugin response: %v", err)
+		}
+	}
+
 	switch ev.Type {
 	case events.Type_TASK_CREATED:
+
 		return b.Submit(ctx, ev.GetTask())
 
 	case events.Type_TASK_STATE:
@@ -136,13 +151,85 @@ func (b *Backend) WriteEvent(ctx context.Context, ev *events.Event) error {
 	return nil
 }
 
+func (b *Backend) UpdateConfig(ctx context.Context) error {
+	resp, ok := ctx.Value("pluginResponse").(*shared.Response)
+	if !ok {
+		return fmt.Errorf("Failed to unmarshal plugin response %v", ctx.Value("pluginResponse"))
+	}
+
+	// TODO: Review all security implications of merging configs (injections, shared state, etc.)
+	if resp.Config != nil {
+		err := b.MergeConfigs(*resp.Config)
+		if err != nil {
+			b.log.Error("error merging configs from plugin", "error", err)
+		}
+	}
+
+	return nil
+}
+
+func (b *Backend) MergeConfigs(c config.Config) error {
+	err := mergo.MergeWithOverwrite(&b.conf, c)
+	if err != nil {
+		return fmt.Errorf("error merging configs: %v", err)
+	}
+
+	return nil
+}
+
 func (b *Backend) Close() {
 	//TODO: close database?
+}
+
+func (b *Backend) createConfigMap(task *tes.Task) (*corev1.ConfigMap, error) {
+	// Load templates
+	cmTpl, err := template.New(task.Id).Parse(b.conf.Kubernetes.ConfigMapTemplate)
+	if err != nil {
+		return nil, fmt.Errorf("parsing template: %v", err)
+	}
+
+	// Template parameters
+	var buf bytes.Buffer
+	err = cmTpl.Execute(&buf, map[string]interface{}{
+		"TaskId":    task.Id,
+		"Namespace": b.namespace,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("executing ConfigMap template: %v", err)
+	}
+
+	decode := scheme.Codecs.UniversalDeserializer().Decode
+	obj, _, err := decode(buf.Bytes(), nil, nil)
+	if err != nil {
+		return nil, fmt.Errorf("decoding ConfigMap spec: %v", err)
+	}
+
+	// Create the ConfigMap that will contain the Funnel Worker Config (`funnel-worker.yaml`)
+	cm, ok := obj.(*corev1.ConfigMap)
+	if !ok {
+		return nil, fmt.Errorf("failed to decode ConfigMap spec")
+	}
+
+	cm.Data = make(map[string]string)
+
+	// TODO: This is a hack to be able to support dynamic namespaces
+	// How can we support this in a more configurable way?
+	// e.g. `funnel-worker.yaml` should be able to reference whatever namespace the Funnel server is in
+	b.conf.Server.HostName = fmt.Sprintf("funnel.%v.svc.cluster.local", b.namespace)
+
+	configBytes, err := config.ToYaml(b.conf)
+	if err != nil {
+		return nil, fmt.Errorf("marshaling config to ConfigMap: %v", err)
+	}
+	cm.Data["funnel-worker.yaml"] = string(configBytes)
+
+	return cm, nil
 }
 
 // Create the Funnel Worker job from kubernetes-template.yaml
 // Executor job is created in worker/kubernetes.go#Run
 func (b *Backend) createJob(task *tes.Task) (*v1.Job, error) {
+	// Parse Worker Template
 	submitTpl, err := template.New(task.Id).Parse(b.template)
 	if err != nil {
 		return nil, fmt.Errorf("parsing template: %v", err)
@@ -248,7 +335,7 @@ func (b *Backend) createPV(task *tes.Task) (*corev1.PersistentVolume, error) {
 
 // Add this helper function for PVC cleanup
 func (b *Backend) deletePVC(ctx context.Context, taskID string) error {
-	clientset, err := kubernetes.NewForConfig(b.config)
+	clientset, err := kubernetes.NewForConfig(b.kubeconfig)
 	if err != nil {
 		return fmt.Errorf("getting kubernetes client: %v", err)
 	}
@@ -268,32 +355,32 @@ func (b *Backend) deletePVC(ctx context.Context, taskID string) error {
 
 // Submit creates both the PVC and the worker job with better error handling
 func (b *Backend) Submit(ctx context.Context, task *tes.Task) error {
-	// Create a new background context instead of inheriting from the potentially canceled one
-	submitCtx := context.Background()
+	clientset, err := kubernetes.NewForConfig(b.kubeconfig)
+	if err != nil {
+		return fmt.Errorf("getting kubernetes client: %v", err)
+	}
+
+	// Initialize ConfigMap
+	cm, err := b.createConfigMap(task)
+	if err != nil {
+		return fmt.Errorf("initializing ConfigMap: %v", err)
+	}
+
+	// Create ConfigMap
+	cm, err = clientset.CoreV1().ConfigMaps(b.namespace).Create(context.Background(), cm, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("creating ConfigMap: %v", err)
+	}
 
 	// TODO: Update this so that a PVC/PV is only created if the task has inputs or outputs
 	// If the task has either inputs or outputs, then create a PVC
 	// shared between the Funnel Worker and the Executor
 	// e.g. `if len(task.Inputs) > 0 || len(task.Outputs) > 0 {}`
-	pvc, err := b.createPVC(task)
-	if err != nil {
-		return fmt.Errorf("creating shared storage PVC: %v", err)
-	}
 
+	// Initialize PV
 	pv, err := b.createPV(task)
 	if err != nil {
-		return fmt.Errorf("creating shared storage PV: %v", err)
-	}
-
-	clientset, err := kubernetes.NewForConfig(b.config)
-	if err != nil {
-		return fmt.Errorf("getting kubernetes client: %v", err)
-	}
-
-	// Create PVC
-	pvc, err = clientset.CoreV1().PersistentVolumeClaims(b.namespace).Create(context.Background(), pvc, metav1.CreateOptions{})
-	if err != nil {
-		return fmt.Errorf("creating PVC: %v", err)
+		return fmt.Errorf("initiaizing PV: %v", err)
 	}
 
 	// Create PV
@@ -302,13 +389,26 @@ func (b *Backend) Submit(ctx context.Context, task *tes.Task) error {
 		return fmt.Errorf("creating PV: %v", err)
 	}
 
-	// Create the worker job
+	// Initialize PVC
+	pvc, err := b.createPVC(task)
+	if err != nil {
+		return fmt.Errorf("initializing PVC: %v", err)
+	}
+
+	// Create PVC
+	pvc, err = clientset.CoreV1().PersistentVolumeClaims(b.namespace).Create(context.Background(), pvc, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("creating PVC: %v", err)
+	}
+
+	// Initialize Worker Job
 	job, err := b.createJob(task)
 	if err != nil {
 		return fmt.Errorf("creating job spec: %v", err)
 	}
 
-	_, err = b.client.Create(submitCtx, job, metav1.CreateOptions{
+	// Create Worker Job
+	_, err = b.client.Create(context.Background(), job, metav1.CreateOptions{
 		FieldManager: task.Id,
 	})
 	if err != nil {

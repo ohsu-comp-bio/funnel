04be4279ea27a9dfe45afcb0c34c37b46f0cdef5
Liam Beckman <lbeckman314@gmail.com>
2025-08-25 16:40:06 -0700

    debug: Github Actions release workflow

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ohsu-comp-bio/funnel/logger"
 )
 
+// https://github.com/ohsu-comp-bio/funnel
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
 		logger.PrintSimpleError(err)

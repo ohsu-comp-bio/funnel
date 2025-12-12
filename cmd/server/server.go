03175e49def6bf8cbe575e4908356219fec1d64b
Liam Beckman <lbeckman314@gmail.com>
2025-12-12 13:14:12 -0800

    chore: Clean up files

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -31,7 +31,7 @@ func newCommandHooks() (*cobra.Command, *hooks) {
 	var (
 		configFile string
 		conf       *config.Config
-		flagConf   *config.Config = config.EmptyConfig()
+		flagConf   *config.Config = config.DefaultConfig()
 	)
 
 	cmd := &cobra.Command{

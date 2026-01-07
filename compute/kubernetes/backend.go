3ec31b23eb47bb278cd4b560a016885244461503
Liam Beckman <lbeckman314@gmail.com>
2026-01-07 13:37:06 -0800

    fix: Return error when creating ServiceAccount

--- compute/kubernetes/backend.go
+++ compute/kubernetes/backend.go
@@ -220,7 +220,8 @@ func (b *Backend) createResources(task *tes.Task, config *config.Config) error {
 			return fmt.Errorf("creating Worker ServiceAccount: %v", err)
 		}
 	} else {
-		b.log.Debug("Error getting ServiceAccount %s", saName, "taskID", task.Id)
+		b.log.Debug("Error getting ServiceAccount:", "ServiceAccount", saName, "taskID", task.Id, "error", err)
+		return fmt.Errorf("error getting ServiceAccount %s for task %s: %v", saName, task.Id, err)
 	}
 
 	// Create Role

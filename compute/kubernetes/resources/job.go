c4f253f2ff81e7a4824620eeb5323b2f8a37733e
geertvandeweyer <geertvandeweyer@gmail.com>
2026-03-19 13:33:53 +0100

    remove accidentally committed build-and-push.sh

--- compute/kubernetes/resources/job.go
+++ compute/kubernetes/resources/job.go
@@ -17,13 +17,13 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 )
 
-// sanitizeLabelValue converts an arbitrary string into a valid Kubernetes label
+// SanitizeLabelValue converts an arbitrary string into a valid Kubernetes label
 // value: replaces any character outside [A-Za-z0-9._-] with '-', strips
 // leading/trailing non-alphanumeric characters, and truncates to 63 chars.
 var labelInvalidChars = regexp.MustCompile(`[^A-Za-z0-9._-]`)
 var labelEdgeTrim = regexp.MustCompile(`^[^A-Za-z0-9]+|[^A-Za-z0-9]+$`)
 
-func sanitizeLabelValue(s string) string {
+func SanitizeLabelValue(s string) string {
 	s = labelInvalidChars.ReplaceAllString(s, "-")
 	if len(s) > 63 {
 		s = s[:63]
@@ -68,7 +68,7 @@ func CreateJob(task *tes.Task, config *config.Config, client kubernetes.Interfac
 	templateData := map[string]interface{}{
 		"TaskId":             task.Id,
 		"TaskName":           task.Name,
-		"TaskNameLabel":      sanitizeLabelValue(task.Name),
+		"TaskNameLabel":      SanitizeLabelValue(task.Name),
 		"Namespace":          config.Kubernetes.Namespace,
 		"JobsNamespace":      config.Kubernetes.JobsNamespace,
 		"Cpus":               res.GetCpuCores(),

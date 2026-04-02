68b9508986d0be963867a678c26fbd3707d977ed
Liam Beckman <lbeckman314@gmail.com>
2026-04-02 16:10:32 -0700

    fix: Add plugin response (`params` field) to safe/redacted config
    Signed-off-by: Liam Beckman <lbeckman314@gmail.com>

--- config/safe.go
+++ config/safe.go
@@ -8,64 +8,62 @@ func (c *Config) Safe() *Config {
 		return nil
 	}
 
-	cloned := proto.Clone(c)
-	safe, ok := cloned.(*Config)
+	safe, ok := proto.Clone(c).(*Config)
 	if !ok {
-		// Fallback: if cloning fails for some unexpected reason, avoid redacting the live config.
 		copy := *c
 		safe = &copy
 	}
 
 	// Database credentials
 	if safe.MongoDB != nil {
-		m := *safe.MongoDB
+		m := proto.Clone(safe.MongoDB).(*MongoDB)
 		m.Password = redact(m.Password)
-		safe.MongoDB = &m
+		safe.MongoDB = m
 	}
 
 	if safe.Postgres != nil {
-		p := *safe.Postgres
+		p := proto.Clone(safe.Postgres).(*Postgres)
 		p.User = redact(p.User)
 		p.Password = redact(p.Password)
 		p.AdminUser = redact(p.AdminUser)
 		p.AdminPassword = redact(p.AdminPassword)
-		safe.Postgres = &p
+		safe.Postgres = p
 	}
 
 	if safe.Elastic != nil {
-		e := *safe.Elastic
+		e := proto.Clone(safe.Elastic).(*Elastic)
 		e.Password = redact(e.Password)
 		e.APIKey = redact(e.APIKey)
 		e.ServiceToken = redact(e.ServiceToken)
-		safe.Elastic = &e
+		safe.Elastic = e
 	}
 
 	// Cloud provider credentials
 	if safe.AWSBatch != nil && safe.AWSBatch.AWSConfig != nil {
-		ab := *safe.AWSBatch
-		ac := *ab.AWSConfig
+		ab := proto.Clone(safe.AWSBatch).(*AWSBatch)
+		ac := proto.Clone(ab.AWSConfig).(*AWSConfig)
 		ac.Key = redact(ac.Key)
 		ac.Secret = redact(ac.Secret)
-		ab.AWSConfig = &ac
-		safe.AWSBatch = &ab
+		ab.AWSConfig = ac
+		safe.AWSBatch = ab
 	}
 
 	if safe.DynamoDB != nil && safe.DynamoDB.AWSConfig != nil {
-		d := *safe.DynamoDB
-		ac := *d.AWSConfig
+		d := proto.Clone(safe.DynamoDB).(*DynamoDB)
+		ac := proto.Clone(d.AWSConfig).(*AWSConfig)
 		ac.Key = redact(ac.Key)
 		ac.Secret = redact(ac.Secret)
-		d.AWSConfig = &ac
-		safe.DynamoDB = &d
+		d.AWSConfig = ac
+		safe.DynamoDB = d
 	}
 
 	if safe.AmazonS3 != nil && safe.AmazonS3.AWSConfig != nil {
-		s3 := *safe.AmazonS3
-		ac := *s3.AWSConfig
+		s3 := proto.Clone(safe.AmazonS3).(*AmazonS3Storage)
+		ac := proto.Clone(s3.AWSConfig).(*AWSConfig)
 		ac.Key = redact(ac.Key)
 		ac.Secret = redact(ac.Secret)
-		s3.AWSConfig = &ac
-		safe.AmazonS3 = &s3
+		s3.AWSConfig = ac
+		safe.AmazonS3 = s3
 	}
 
 	if safe.GenericS3 != nil {
@@ -73,74 +71,83 @@ func (c *Config) Safe() *Config {
 			if s3 == nil {
 				continue
 			}
-			gs3 := *s3
+			gs3 := proto.Clone(s3).(*GenericS3Storage)
 			gs3.Key = redact(gs3.Key)
 			gs3.Secret = redact(gs3.Secret)
-			safe.GenericS3[i] = &gs3
+			safe.GenericS3[i] = gs3
 		}
 	}
 
 	if safe.PubSub != nil {
-		ps := *safe.PubSub
+		ps := proto.Clone(safe.PubSub).(*PubSub)
 		ps.CredentialsFile = redact(ps.CredentialsFile)
-		safe.PubSub = &ps
+		safe.PubSub = ps
 	}
 
 	if safe.Datastore != nil {
-		ds := *safe.Datastore
+		ds := proto.Clone(safe.Datastore).(*Datastore)
 		ds.CredentialsFile = redact(ds.CredentialsFile)
-		safe.Datastore = &ds
+		safe.Datastore = ds
 	}
 
 	if safe.GoogleStorage != nil {
-		gs := *safe.GoogleStorage
+		gs := proto.Clone(safe.GoogleStorage).(*GoogleCloudStorage)
 		gs.CredentialsFile = redact(gs.CredentialsFile)
-		safe.GoogleStorage = &gs
+		safe.GoogleStorage = gs
 	}
 
 	// Auth credentials
 	if safe.Server != nil {
-		s := *safe.Server
+		s := proto.Clone(safe.Server).(*Server)
 
 		if s.BasicAuth != nil {
 			for i, cred := range s.BasicAuth {
 				if cred == nil {
 					continue
 				}
-				bc := *cred
+				bc := proto.Clone(cred).(*BasicCredential)
 				bc.Password = redact(bc.Password)
-				s.BasicAuth[i] = &bc
+				s.BasicAuth[i] = bc
 			}
 		}
 
 		if s.OidcAuth != nil {
-			oa := *s.OidcAuth
+			oa := proto.Clone(s.OidcAuth).(*OidcAuth)
 			oa.ClientSecret = redact(oa.ClientSecret)
-			s.OidcAuth = &oa
+			s.OidcAuth = oa
 		}
 
-		safe.Server = &s
+		safe.Server = s
 	}
 
 	if safe.RPCClient != nil && safe.RPCClient.Credential != nil {
-		rpc := *safe.RPCClient
-		cred := *rpc.Credential
+		rpc := proto.Clone(safe.RPCClient).(*RPCClient)
+		cred := proto.Clone(rpc.Credential).(*BasicCredential)
 		cred.Password = redact(cred.Password)
-		rpc.Credential = &cred
-		safe.RPCClient = &rpc
+		rpc.Credential = cred
+		safe.RPCClient = rpc
 	}
 
 	// Storage credentials
 	if safe.Swift != nil {
-		sw := *safe.Swift
+		sw := proto.Clone(safe.Swift).(*SwiftStorage)
 		sw.Password = redact(sw.Password)
-		safe.Swift = &sw
+		safe.Swift = sw
 	}
 
 	if safe.FTPStorage != nil {
-		ftp := *safe.FTPStorage
+		ftp := proto.Clone(safe.FTPStorage).(*FTPStorage)
 		ftp.Password = redact(ftp.Password)
-		safe.FTPStorage = &ftp
+		safe.FTPStorage = ftp
+	}
+
+	if safe.Plugins != nil && safe.Plugins.Params != nil {
+		for i, param := range safe.Plugins.Params {
+			if param == "" {
+				continue
+			}
+			safe.Plugins.Params[i] = redact(param)
+		}
 	}
 
 	return safe
